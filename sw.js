6d602949b4d447f9c1cb0fb7441adbe094a8d677
Kishan Kumar <kishan.kumar@intimetec.com>
2019-08-22 12:21:46 +0530

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0251b3ba40db230e4e72.js"
+    "url": "webpack-runtime-02b1d79e9b970b5423f3.js"
   },
   {
     "url": "app-4ba8a14466c02a5265f9.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "72336fc8e79764f3dfc519582253e23f"
+    "revision": "a7fadf04832a5abfbcde73c45c53ea66"
   },
   {
     "url": "manifest.json",
